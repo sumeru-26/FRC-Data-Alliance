c30201bedc5953597a2bac8a43285c111c8135d8
Sumeru Gowda <155037091+sumeru-26@users.noreply.github.com>
2024-03-20 00:44:32 +0000

    FIX

--- fetch-data.js
+++ fetch-data.js
@@ -1,7 +1,15 @@
+import { TBA_KEY } from "./config";
+
 export async function get_data() {
     //just a test url
     const url = "https://www.thebluealliance.com/api/v3/status";
-    const response = await fetch(url);
+    const request = new Request(url, {
+        method: "GET",
+        headers: {
+            "X-TBA-Auth-Key": TBA_KEY
+        }
+    });
+    reponse = fetch(request);
     const data = await response.json();
     return data;
 }
